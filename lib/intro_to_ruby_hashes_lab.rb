49a6780beebcb6d99f3431f7d6dc2df7102d91a9
Brian Rhodes <brianr4548@gmail.com>
2020-10-05 23:05:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,33 +3,15 @@ def new_hash
 end
 
 def my_hash
-<<<<<<< HEAD
    {:bob => "blue"}
 end
 
 def pioneer
   {:name => "Grace Hopper"}
-=======
-  {
-    :bob => "blue"
-  }
-end
 
-def pioneer
-  {
-    :name => "Grace Hopper"
-  }
->>>>>>> be30c2b75eb0c0bc51587f9106d83cc76e984d1b
 end
 
 
 def id_hash_generator(number)
-<<<<<<< HEAD
   {id: number}
-=======
-  {
-    id: number
-    
-  }
->>>>>>> be30c2b75eb0c0bc51587f9106d83cc76e984d1b
 end
